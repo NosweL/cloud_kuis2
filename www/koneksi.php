d5a343965efdb6a449c9c9f05fb8e24c222e8224
noswel <noswelvr@gmail.com>
2022-11-25 21:13:06 +0700

    CRUD Cloud Kuis2

--- www/koneksi.php
+++ www/koneksi.php
@@ -3,7 +3,7 @@ $server = "db";
 $username = "root";
 $pass = "welson";
 $db = "db_crud";
-$db = mysqli_connect($server, $username, $pass, $db);
+$koneksi = mysqli_connect($server, $username, $pass, $db);
 // echo "berhasil terkoneksi..... ";
 if (mysqli_connect_errno()) {
 	echo "Koneksi gagal : " . mysqli_connect_error();
